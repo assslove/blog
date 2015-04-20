009d40c1bcbeb9522525fe3485eb8a66f07a2889
houbin <houbin-12@163.com>
2015-04-20 19:43:30 +0800

    增加备份库

--- src/sql/bak.sh
+++ src/sql/bak.sh
@@ -1,7 +1,8 @@
 # 备份远程
-# 备份mysql 全库
-ssh 5532215afcf933acf700000f@xxmn-limai.rhcloud.com 'mysqldump -uadminkt2Nj93 -ptkV8Rpma-M1G blog > blog.sql'
 
+ssh 5532215afcf933acf700000f@xxmn-limai.rhcloud.com 'mysqldump -h127.5.31.130 -P3306 -uadminkt2Nj93 -ptkV8Rpma-M1G blog > blog.sql'
+echo "back blog";
 # 备份图片为库
 ssh 5532215afcf933acf700000f@xxmn-limai.rhcloud.com 'tar -zcvf upload.tar.gz ./../../img/upload/'
+echo "back img";
 
