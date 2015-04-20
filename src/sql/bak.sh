6c5c337a29f8fea318189a380857bb091c238a39
houbin <houbin-12@163.com>
2015-04-20 19:46:30 +0800

    add some

--- src/sql/bak.sh
+++ src/sql/bak.sh
@@ -1,8 +1,8 @@
 # 备份远程
 
-ssh 5532215afcf933acf700000f@xxmn-limai.rhcloud.com 'mysqldump -h127.5.31.130 -P3306 -uadminkt2Nj93 -ptkV8Rpma-M1G blog > blog.sql'
+mysqldump -h127.5.31.130 -P3306 -uadminkt2Nj93 -ptkV8Rpma-M1G blog > blog.sql
 echo "back blog";
 # 备份图片为库
-ssh 5532215afcf933acf700000f@xxmn-limai.rhcloud.com 'tar -zcvf upload.tar.gz ./../../img/upload/'
+tar -zcvf upload.tar.gz ./../../img/upload/
 echo "back img";
 
--- src/sql/publish.sh
+++ src/sql/publish.sh
@@ -1,11 +1,8 @@
 #一键发布功能
 
 # 备份mysql 全库
-ssh 5532215afcf933acf700000f@xxmn-limai.rhcloud.com 'mysqldump -h127.5.31.130 -P3306 -uadminkt2Nj93 -ptkV8Rpma-M1G blog > blog.sql'
-echo "back blog";
-# 备份图片为库
-ssh 5532215afcf933acf700000f@xxmn-limai.rhcloud.com 'tar -zcvf upload.tar.gz ./../../img/upload/'
-echo "back img";
+ssh 5532215afcf933acf700000f@xxmn-limai.rhcloud.com 'sh ./bak.sh'
+echo "remote back sql & img";
 
 #拷贝到本地
 scp 5532215afcf933acf700000f@xxmn-limai.rhcloud.com:~/app-root/repo/src/sql/blog.sql .
