252a8ca124fff50ca61ef32ab9bfdabbcdd9ed1c
houbin <houbin-12@163.com>
2015-04-21 09:52:34 +0800

    add some

--- src/sql/publish.sh
+++ src/sql/publish.sh
@@ -10,7 +10,7 @@ scp 5532215afcf933acf700000f@xxmn-limai.rhcloud.com:~/app-root/repo/src/sql/uplo
 echo "cpy to local";
 
 #拷贝到工作目录 并解压照片
-cp upload.tar.gz /var/www/html/blog/img && cd /var/www/html/img && tar -zxvf upload.tar.gz
+cp upload.tar.gz /var/www/html/blog/img && cd /var/www/html/blog/img && tar -zxvf upload.tar.gz
 #照片加入本地库中
 git add *
 git commit -a -m "统一提交"
