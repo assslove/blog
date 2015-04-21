0a4bab0f34416a3b1daa8cacb7812bfc21363250
houbin <houbin-12@163.com>
2015-04-21 09:57:35 +0800

    add some

--- src/sql/bak.sh
+++ src/sql/bak.sh
@@ -3,6 +3,6 @@
 mysqldump -h127.5.31.130 -P3306 -uadminkt2Nj93 -ptkV8Rpma-M1G blog > blog.sql
 echo "back blog";
 # 备份图片为库
-tar -zcvf upload.tar.gz ./../../img/upload/
+tar -zcv upload.tar.gz ./../../img/upload/*
 echo "back img";
 
